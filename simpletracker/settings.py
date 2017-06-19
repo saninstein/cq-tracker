be54d83185734b965f43fa3d5a181003061a6928
Alex <saninstein@yandex.ua>
2017-06-19 16:49:20 +0300

    date items

--- simpletracker/settings.py
+++ simpletracker/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'simpletracker.wsgi.application'
 DATABASES = {
   'default': {
       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      'NAME': 'djangostack',
+      'NAME': 'DB',
       'HOST': '/opt/bitnami/postgresql',
       'PORT': '5432',
       'USER': 'user_tracker',
